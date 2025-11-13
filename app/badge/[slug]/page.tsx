c8338be1271a98cca241ccffc8a0f53d06961f65
Sabbaghh <afsabbagh9@gmail.com>
2025-11-13 14:49:23 +0400

    Fix alignment

--- app/badge/[slug]/page.tsx
+++ app/badge/[slug]/page.tsx
@@ -66,8 +66,8 @@ export default function BadgePage() {
       <div className="flex w-full h-1/2 flex-row">
         <div className="flex-1 relative flex flex-col justify-center items-center">
           <Image src="/Bagde.svg" alt="badge" fill objectFit="contain" />
-          <div className="w-3/4  h-12 flex justify-center items-center mt-[50%] mb-[20%] ">
-            <p className="text-black text-2xl font-bold capitalize">
+          <div className="w-3/4  h-12 flex justify-center items-center mt-[50%] mb-[30%] ">
+            <p className="text-black text-center text-2xl font-bold capitalize">
               {user.name}
             </p>
           </div>
